d8bf8b9b4dcf4c6f162684ef9cea868d76c51807
ThiagoFaria <thiago_faria@outlook.com>
2020-04-13 23:58:15 -0300

    Mais de uma variavel

---
+++ variaveis/exemplo02.php
@@ -0,0 +1,10 @@
+<?php
+
+$Nome_Completo    =   "Thiago Faria"; 
+$AnoNascimento   =   "1987";
+
+//Exibe o conteudo da variavel
+echo "Nome Completo: $Nome_Completo <br /> <br /> Ano Nascimento: $AnoNascimento <br />";
+
+
+?>

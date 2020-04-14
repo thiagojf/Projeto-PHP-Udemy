64b5c63cfb3e332f2a05885516a554dcb7e44ae5
ThiagoFaria <thiago_faria@outlook.com>
2020-04-14 00:09:19 -0300

    Concatenação de variavel

--- variaveis/exemplo02.php
+++ variaveis/exemplo02.php
@@ -1,10 +1,22 @@
 <?php
 
-$Nome_Completo    =   "Thiago Faria"; 
+$Nome = "Thiago";
+$SobreNome = "Faria";
+$Nome_Completo    =   $Nome . " " . $SobreNome; 
 $AnoNascimento   =   "1987";
 
 //Exibe o conteudo da variavel
 echo "Nome Completo: $Nome_Completo <br /> <br /> Ano Nascimento: $AnoNascimento <br />";
+echo "<br />";
 
+//Limpa a variavel
+//unset($AnoNascimento);
+
+/* so executa a variavel se ela existir
+if (isset($AnoNascimento)) {
+
+    echo "Nome Completo: $Nome_Completo <br /> <br /> Ano Nascimento: $AnoNascimento <br />";
+
+}*/
 
 ?>

dbc7d27dd2bb82cdbf52d4273ac53720cd84abaa
ThiagoFaria <thiago_faria@outlook.com>
2020-04-14 01:04:44 -0300

    Variaveis pré-definidas

--- variaveis/exemplo04.php
+++ variaveis/exemplo04.php
@@ -1,14 +1,18 @@
 <?php 
 
-$nome = $_GET["A"]
+$V_a = (int)$_GET["a"];
+$V_b = (int)$_GET["b"];
 
-var_dump($nome)
-
-
-
-
+$ip = $_SERVER["REMOTE_ADDR"];
+$nane = $_SERVER['SERVER_NAME'];;
 
 
-
+echo "Variavel A: " . $V_a;
+echo "<br />";
+echo "Variavel B: " . $V_b;
+echo "<br />";
+echo $ip;
+echo "<br />";
+echo $nane;
 
 ?>

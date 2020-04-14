23bb513022ad738bbd8b52a28c06f00218291df7
ThiagoFaria <thiago_faria@outlook.com>
2020-04-13 23:05:04 -0300

    Inserido a data de 13/04/2020

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
     echo "Hellow Word, !!<strong> Meu primeiro programa</strong>";
     echo "Thiago Faria";
-    echo "12/04/2020"
+    echo "12/04/2020 as 21:03"
+    echo "13/04/2020 as 23:00"
     //nova descrição
 
 ?>

cd6da5f85f51e04db433e50b43cb6c98d4480b16
ThiagoFaria <thiago_faria@outlook.com>
2020-04-16 23:49:06 -0300

    Include e require once

--- include/exemplo02.php
+++ include/exemplo02.php
@@ -1,8 +1,10 @@
 <?php
 // O include não obriga que o arquivo esteja funcionado corretamente
-include "exemplo01.php";
+//include "exemplo01.php";
 // o require espera que o arquivo esteja 100% ok, caso contrario ele para a execusão
-require "exemplo01.php"
+//require "exemplo01.php";
+//inclui apenas uma vez o arquivo de inclusão e obriga que esteja 100%
+require_once "exemplo01.php";
 
 $resultado = somar(10, 20);
 

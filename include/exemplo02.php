cabb4e3c5b3c21930302ab473589d68a09da9188
ThiagoFaria <thiago_faria@outlook.com>
2020-04-16 23:44:24 -0300

    Include e Require

--- include/exemplo02.php
+++ include/exemplo02.php
@@ -1,6 +1,8 @@
 <?php
-
+// O include não obriga que o arquivo esteja funcionado corretamente
 include "exemplo01.php";
+// o require espera que o arquivo esteja 100% ok, caso contrario ele para a execusão
+require "exemplo01.php"
 
 $resultado = somar(10, 20);
 

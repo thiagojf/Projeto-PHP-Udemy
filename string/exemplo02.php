9fb62957cef31f018b84f4c0d499e08450671577
ThiagoFaria <thiago_faria@outlook.com>
2020-04-16 22:51:01 -0300

    Conversão de string

---
+++ string/exemplo02.php
@@ -0,0 +1,20 @@
+<?php
+
+$nome = "thiago faria";
+$nome2 = "THIAGO";
+
+// converte em maiusculo
+echo strtoupper($nome);
+echo "<br>";
+// Converte em minusculo
+echo strtolower($nome);
+echo "<br>";
+//Coloca cada letra inicial em maiuscula
+echo ucwords($nome);
+echo "<br>";
+// Converte a primeira letra para maiusculo da primeira palavra
+echo ucfirst($nome);
+echo "<br>";
+
+
+?>

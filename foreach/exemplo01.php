fc2b8f6a9abd36201f7538f84213181d60c300d8
ThiagoFaria <thiago_faria@outlook.com>
2020-04-23 22:50:58 -0300

    Laços de repetição

---
+++ foreach/exemplo01.php
@@ -0,0 +1,23 @@
+<?php 
+/** 
+ 
+*/
+ 
+$meses = array(
+
+    "Janeiro", "Fevereiro", "Março",
+    "Abril", "Maio", "junho",
+    "Julho", "Agosto", "Setembro",
+    "Outubo", "Novembro", "Dezembro"
+
+);
+ 
+foreach ($meses as $index => $mes) {
+    echo "Indice: ".$index."<br>";
+    echo "o més é: ".$mes."<br>";
+}
+
+
+
+ 
+?>
---
+++ while/exemplo01.php
@@ -0,0 +1,18 @@
+<?php 
+/** 
+ 
+*/
+
+$condição = true;
+ 
+ while ($condição) {
+     $numero = rand(1, 10);
+     if ($numero === 3) {
+        echo "TRÊS !!";
+        $condição = false;
+     }
+
+     echo $numero ." "; 
+ }
+ 
+?>
